44a23eb2bc8224944ea1f998486dc33d801d3e61
João Pedro Peres Bertoncelo <114821140+Joaobertoncelo@users.noreply.github.com>
2024-04-24 15:21:57 -0300

    NPCs movement and AI Finished

--- Assets/Scripts/NPCs/NPC_Movement.cs
+++ Assets/Scripts/NPCs/NPC_Movement.cs
@@ -6,18 +6,39 @@ using UnityEngine;
 public class NPC_Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private float initialSpeed;
 
     private int nextPath;
+    private Animator npcAnimation;
 
     [SerializeField] private List<Transform> possiblePaths = new List<Transform>();
 
+    private void Start()
+    {
+        initialSpeed = speed;
+        npcAnimation = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        if(DialogueController.instance.isDialogueVisible)
+        {
+            speed = 0f;
+            npcAnimation.SetBool("isWalking", false);
+        }
+        else
+        {
+            speed = initialSpeed;
+            npcAnimation.SetBool("isWalking", true);
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, possiblePaths[nextPath].position, speed* Time.deltaTime);
+
         if(Vector2.Distance(transform.position, possiblePaths[nextPath].position) < 0.1f)
         {
-            if(nextPath < possiblePaths.Count - 1)
+            if (nextPath < possiblePaths.Count - 1)
             {
+                //nextPath = Random.Range(0, possiblePaths.Count - 1);
                 nextPath++;
             }
             else
