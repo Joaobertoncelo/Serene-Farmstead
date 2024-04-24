d2746d33c22a55e6f0b4db485b7361756f0b09d8
João Pedro Peres Bertoncelo <114821140+Joaobertoncelo@users.noreply.github.com>
2024-04-24 11:11:22 -0300

    World Creation, Movimentation and First NPC added

---
+++ Assets/Scripts/NPCs/Dialogue/DialogueSettings.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "New Dialogue/Dialogue")]
+public class DialogueSettings : ScriptableObject
+{
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialogue")]
+    public Sprite speakerSprite;
+    public string actorText;
+
+    public List<Sentences> ActorSpeech = new List<Sentences>();
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite actorProfilePicture;
+    public Languages language;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
+    public string french;
+    public string german;
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DialogueSettings))]
+public class BuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogueSettings dialogueSettings = (DialogueSettings)target;
+
+        Languages language = new Languages();
+        language.portuguese = dialogueSettings.actorText;
+
+        Sentences sentence = new Sentences();
+        sentence.actorProfilePicture = dialogueSettings.speakerSprite;
+        sentence.language = language;
+
+        if(GUILayout.Button("create Dialogue"))
+        {
+            if(dialogueSettings.actorText != "")
+            {
+                dialogueSettings.ActorSpeech.Add(sentence);
+
+                dialogueSettings.speakerSprite = null;
+                dialogueSettings.actorText = "";
+            }
+        }
+    }
+}
+
+
+#endif
