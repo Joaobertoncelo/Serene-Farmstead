601893318312a616ec97f6a18b2d60078dfd4ae1
João Pedro Peres Bertoncelo <114821140+Joaobertoncelo@users.noreply.github.com>
2024-04-24 14:24:52 -0300

    NPCs Paths added
    Now any NPC can follow the paths added on the map

--- Assets/Scripts/NPCs/NPC_Movement.cs
+++ Assets/Scripts/NPCs/NPC_Movement.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NPC_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
 
-    // Update is called once per frame
+    private int nextPath;
+
+    [SerializeField] private List<Transform> possiblePaths = new List<Transform>();
+
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, possiblePaths[nextPath].position, speed* Time.deltaTime);
+        if(Vector2.Distance(transform.position, possiblePaths[nextPath].position) < 0.1f)
+        {
+            if(nextPath < possiblePaths.Count - 1)
+            {
+                nextPath++;
+            }
+            else
+            {
+                nextPath = 0;
+            }
+        }
+
+        Vector2 npcDirection = possiblePaths[nextPath].position - transform.position;
+
+        if (npcDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (npcDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
