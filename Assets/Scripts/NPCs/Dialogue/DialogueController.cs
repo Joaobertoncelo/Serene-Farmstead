a59fd2e6a5f0c782a2ed95f18cb33f03f7943694
João Pedro Peres Bertoncelo <114821140+Joaobertoncelo@users.noreply.github.com>
2024-04-24 11:38:03 -0300

    Initial Dialogue Settings
    Dialogue-writing implemented and functional, player has now a "Player" layer, that's gonna be detected when it's close to NPCs

--- Assets/Scripts/NPCs/Dialogue/DialogueController.cs
+++ Assets/Scripts/NPCs/Dialogue/DialogueController.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Components")]
+    public GameObject dialogueObject;
+    public Image actorProfilePicture;
+    public Text speechText;
+    public Text actorName;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    private bool isDialogueVisible;
+    private int index;
+    private string[] currentSentences;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in currentSentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+    }
+
+    public void Speech(string[] txt)
+    {
+        if(!isDialogueVisible)
+        {
+            dialogueObject.SetActive(true);
+            currentSentences = txt;
+            StartCoroutine(TypeSentence());
+            isDialogueVisible = true;
+        }
     }
 }
