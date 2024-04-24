00a8e5e0d49cd52bdc9717b991ad938bbd2cd259
João Pedro Peres Bertoncelo <114821140+Joaobertoncelo@users.noreply.github.com>
2024-04-24 11:48:00 -0300

    Player detection area
    Implemented the area where the player can interact with the NPC, tests where made using "Debug.log("player detected")".

--- Assets/Scripts/NPCs/NPC_Dialogue.cs
+++ Assets/Scripts/NPCs/NPC_Dialogue.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NPC_Dialogue : MonoBehaviour
 {
+    public float dialogueRange;
+    public LayerMask PlayerLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,27 @@ public class NPC_Dialogue : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D playerDetector = Physics2D.OverlapCircle(transform.position, dialogueRange, PlayerLayer);
         
+        if (playerDetector != null)
+        {
+            Debug.Log("Player Detected");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
     }
 }
